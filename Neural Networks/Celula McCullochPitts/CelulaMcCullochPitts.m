56f41d168558c84ce76e8299123f9e25219c3310
Garz4 <fancygarz4@gmail.com>
2023-03-13 18:37:56 -0600

    Update LICENSE

--- Neural Networks/Celula McCullochPitts/CelulaMcCullochPitts.m
+++ Neural Networks/Celula McCullochPitts/CelulaMcCullochPitts.m
@@ -1,6 +1,6 @@
 %% MIT License
 %%
-%% Copyright (c) 2021 Uriel Rivas
+%% Copyright (c) 2023 Uriel Rivas
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a copy
 %% of this software and associated documentation files (the "Software"), to deal
@@ -8,8 +8,11 @@
 %% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 %% copies of the Software, and to permit persons to whom the Software is
 %% furnished to do so, subject to the following conditions:
+%% 
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
 %%
-%% https://github.com/Garz4/artificial-intelligence/blob/master/LICENSE
+%% https://github.com/Garz4/artificial-intelligence/blob/main/LICENSE
 
 % Programado por: Uriel Garcia Rivas
 % Neural Networks
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# ArtificialIntelligence
+# AI
 
 All my coursework from "Foundations of Artificial Intelligence" and "Neural Networks" courses.
+
+# Neural Networks
+
+# Microworlds
+
+# Negamax algorithm
